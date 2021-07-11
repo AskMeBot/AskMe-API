d645abf7244a6afe6691a5f008ccc7ad0a88cade
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-07-11 02:51:55 -0700

    Update v1.ts

--- src/api/v1.ts
+++ src/api/v1.ts
@@ -4,8 +4,13 @@ import { Router } from "express";
 const router = Router()
 
 router.get("/trivia/telegram", (req, res, next) => {
-    let json = JSON.parse(readFileSync("./trivia.json").toString())
-    json = json.filter(t => t && !t.guild_id)
+    let json:TriviaQuestion[] = JSON.parse(readFileSync("./trivia.json").toString())
+    json = json.filter(t => t && !t.guild_id && !t.requireAllAnswersSelected)
+    res.json(json)
+})
+
+router.get("/trivia/discord", (req, res, next) => {
+    let json:TriviaQuestion[] = JSON.parse(readFileSync("./trivia.json").toString())
     res.json(json)
 })
 
