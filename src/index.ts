8700a79fdcb46f8ad2258a35e0eb3d097db10685
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-07-12 00:36:09 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
+import config from '../config'
 import express from 'express';
 import v1API from './api/v1'
 const app = express();
 const port = process.env.PORT || 3000;
+const requiredAuthHeader = process.env.requiredAuthHeader || config.requiredAuthHeader
+
+app.use((req, res, next) => {
+  if(!req.headers.authorization || req.headers.authorization != requiredAuthHeader)
+    return res.status(401).json({message:"Unauthorized"})
+  next()
+})
 
 //Use API V1 on route /api/v1
 app.use("/api/v1", v1API)
