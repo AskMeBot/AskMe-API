b3ff38ca5d4a39327af45574a79a12b5d72a4e39
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2022-03-27 12:12:00 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import express from 'express';
 import v1API from './api/v1'
+import cors from 'cors'
+
 const app = express();
 const port = process.env.PORT || 3000;
-const requiredAuthHeader = process.env.requiredAuthHeader
+const requiredAuthHeader = process.env.requiredAuthHeader || false
+
+app.use(cors())
+app.use(express.json({
+  limit:"2mb"
+}))
 
 app.use((req, res, next) => {
   if(!req.headers.authorization || !requiredAuthHeader || req.headers.authorization != requiredAuthHeader)
